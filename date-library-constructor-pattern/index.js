a9aeafdc9fe4f4938dc2c5e516dfd64c0e6d460c
danleks <danleks@pop-os.localdomain>
2022-01-30 21:11:44 +0100

    update the project

--- date-library-constructor-pattern/index.js
+++ date-library-constructor-pattern/index.js
@@ -2,36 +2,54 @@ import Time from './time.js';
 
 // Create a new Time() instance
 let halloween = new Time('October 31, 2021');
-// console.log(halloween2);
 
-// This should also work
-let halloween2 = new Time(1970, 9, 31);
+// If the year on the Time() instance is greater than 2021, don't update
+document.addEventListener('time:update', function (event) {
+    // Update going to happen
+	console.log('update going to happen', event.detail);
+    // if current year greater than 2021, don't update
+	if (event.detail.time.date.getFullYear() > 2021) {
+		event.preventDefault();
+        console.log('event is cancelled', event.detail);
+        return;
+	}
+
+    // update happened
+    console.log('event completed', event.detail);
+});
+
+// Adjust the date
+// the addYears() method won't update the date in this instance
+halloween.addDays(3).addMonths(1).addYears(1);
+
+console.log(halloween);
 
 
-// Get the date object
-let date = halloween2.date;
-console.log(date);
 
 
-// returns "Sunday"
-let day = halloween.getDay();
-console.log(day);
 
+// Create a new Time() instance
+// Customize the days and months
+// let halloween = new Time('October 31, 2021', {
+// 	days: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+// 	months: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+// });
 
-// returns "October"
-let month = halloween.getMonth();
-console.log(month);
+// Try to override settings
+// These should not change anything
+// halloween._settings = null;
+// halloween._settings.months = null;
 
+// Get details from the instance
+// returns "domingo" and "octubre"
+// let day = halloween.getDay();
+// let month = halloween.getMonth();
+// console.log(day, month);
 
 // Add some time
-halloween.addDays(3).addMonths(1).addYears(5);
+//let allHallowsDay = halloween.addYears(5).addDays(1);
 
-// returns "Thursday"
-let newDay = halloween.getDay();
-console.log(newDay);
-
-
-// returns "December"
-let newMonth = halloween.getMonth();
-console.log(newMonth);
-
+// halloween should still be October 31, 2021
+// allHallowsDay should be November 1, 2026
+// console.log(halloween.date);
+// console.log(allHallowsDay.date);
--- date-library-constructor-pattern/time.js
+++ date-library-constructor-pattern/time.js
@@ -1,24 +1,67 @@
 const Time = (() => {
-    function Constructor(...date) {
-        this.date = new Date(date.join(','));
+    /**
+     * Emit a custom event
+     * @param  {String} type   The event type
+     * @param  {Object} detail Any details to pass along with the event
+     * @param  {Node}   elem   The element to attach the event to
+    */
+    function emitEvent(type, detail = {}, elem = document) {
+        // make sure there's an event type
+        if (!type) return;
+    
+        // create a new custom event
+        let event = new CustomEvent(type, {
+            bubbles: true,
+            cancelable: true,
+            detail: detail,
+        });
+
+        return elem.dispatchEvent(event);
+    };
+
+    /**
+     * The Constructor object
+     * @param {Array}  date    A date string, object, or array of arguments
+     * @param {Object} options Options and settings
+    */
+    function Constructor(date = [], options = {}) {
+        let settings = Object.assign({
+            days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        }, options)
+
+        // check if date parameter is not array and if not, make it arr
+        if (!Array.isArray(date)) {
+            date = [date];
+        }
+       
+        // freeze settings object so it and its properties cannot be modified
+        Object.freeze(settings);
+        
+        // define instance properties
+        Object.defineProperties(this, {
+            date: {value: new Date(...date)},
+            _settings: {value: settings},
+        })
+
+        // emit a time:ready event
+        emitEvent('time:ready', {
+            time: this
+        })
     };
 
     /**
     * get the day of the week as a string.
     */
     Constructor.prototype.getDay = function() {
-        // An array of day names
-        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        return days[this.date.getDay()];
+        return this._settings.days[this.date.getDay()];
     };
 
     /**
     * get the month of the year as a string.
     */
     Constructor.prototype.getMonth = function() {
-        // An array of month names
-        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        return months[this.date.getMonth()];
+        return this._settings.months[this.date.getMonth()];
     }
 
     /**
@@ -26,8 +69,22 @@ const Time = (() => {
     * @param {Integer} n    The number of seconds to add
     */
     Constructor.prototype.addSeconds = function(n) {
-        this.date.setSeconds(this.date.getSeconds() + n);
-        return this;
+        let d = new Date(this.date);
+        d.setSeconds(d.getSeconds() + n);
+        let time = new Constructor(d, this._settings);
+
+        // Emit time:update event
+        let cancel = !emitEvent('time:update', {
+            time,
+            type: 'seconds',
+            amount: n,
+        })
+
+        // If canceled, return a copy of the original instance
+	    // If not, return new instance
+        if (cancel) return new Constructor(this.date, this._settings);
+        return time;
+        // return new Constructor(d, this._settings);
     }
 
     /**
@@ -35,8 +92,18 @@ const Time = (() => {
     * @param {Integer} n    The number of minutes to add
     */
     Constructor.prototype.addMinutes = function(n) {
-        this.date.setMinutes(this.date.getMinutes() + n);
-        return this;
+        let d = new Date(this.date);
+        d.setMinutes(d.getMinutes() + n)
+        let time = new Constructor(d, this._settings);
+        
+        let cancel = !emitEvent('time:update', {
+            time,
+            type: 'minutes',
+            amount: n
+        })
+
+        if (cancel) return new Constructor(this.date, this._settings);
+        return time;
     }
 
     /**
@@ -44,8 +111,18 @@ const Time = (() => {
     * @param {Integer} n    The number of hours to add
     */
     Constructor.prototype.addHours = function(n) {
-        this.date.setHours(this.date.getHours() + n);
-        return this;
+        let d = new Date(this.date);
+        d.setHours(d.getHours() + n)
+        let time = new Constructor(d, this._settings);
+
+        let cancel = !emitEvent('time:update', {
+            time,
+            type: 'hours',
+            amount: n
+        });
+
+        if (cancel) return new Constructor(this.date, this._settings);
+        return time;
     }
 
     /**
@@ -53,8 +130,18 @@ const Time = (() => {
     * @param {Integer} n    The number of days to add
     */
     Constructor.prototype.addDays = function(n) {
-        this.date.setDate(this.date.getDate() + n);
-        return this;
+        let d = new Date(this.date);
+        d.setDate(d.getDate() + n)
+        let time = new Constructor(d, this._settings);
+
+        let cancel = !emitEvent('time:update', {
+            time,
+            type: 'days',
+            amount: n
+        });
+
+        if (cancel) return new Constructor(this.date, this._settings);
+        return time;
     }
 
     /**
@@ -62,8 +149,18 @@ const Time = (() => {
      * @param {Integer} n    The number of months to add
      */
     Constructor.prototype.addMonths = function(n) {
-        this.date.setMonth(this.date.getMonth() + n);
-        return this;
+        let d = new Date(this.date);
+        d.setMonth(d.getMonth() + n)
+        let time = new Constructor(d, this._settings);
+
+        let cancel = !emitEvent('time:update', {
+            time,
+            type: 'months',
+            amount: n
+        });
+
+        if (cancel) return new Constructor(this.date, this._settings);
+        return time;
     }
 
     /**
@@ -71,8 +168,18 @@ const Time = (() => {
      * @param {Integer} n    The number of months to add
      */
     Constructor.prototype.addYears = function(n) {
-        this.date.setFullYear(this.date.getFullYear() + n);
-        return this;
+        let d = new Date(this.date);
+        d.setFullYear(d.getFullYear() + n)
+        let time = new Constructor(d, this._settings);
+
+        let cancel = !emitEvent('time:update', {
+            time,
+            type: 'years',
+            amount: n
+        });
+
+        if (cancel) return new Constructor(this.date, this._settings);
+        return time;
     }
 
     return Constructor;
